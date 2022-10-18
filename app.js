38bb55b5af4ce804b84e92eea953f15374f74fcc
Hua0720 <stella573021@gmail.com>
2022-10-19 02:29:48 +0800

    add makeURL page  and error page

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const exphbs = require('express-handlebars');
 
 const mongoose = require('mongoose') // 載入 mongoose
 
-const shorten = require('../../utilities/URL')
-const URL = require('../../models/URL')
+const shortenURL = require('../../utilities/shorten')
+const makeURL = require('../../models/URL')
 
 
 const app = express()
@@ -46,7 +46,40 @@ app.get('/', (req, res) => {
 
 // 新增URL網址
 app.post('/', (req, res) => {
-  
+  // 沒有按下製作鍵只會停留於首頁
+  if (!req.body.url) return res.redirect('/')
+  // 產生隨機碼
+  const shortURL = shortenURL(5)
+  makeURL.findOne({ originalUrl: req.body.url })
+    .lean()
+    // 產生新縮址
+    .then(data => data ? data : makeURL.create({
+      shortURL, 
+      originalUrl: req.body.url
+      })
+    )
+    // 相同網址，產生相同的縮址
+    .then(data => res.render('index', {
+      origin: req.headers.origin,
+      shortURL: data.shortURL
+      })
+    )
+    .catch(err => console.log(err))
+})
+
+// 將縮址導回原本網站
+app.get('/:shortURL', (req, res) => {
+  const { shortURL } = req.params
+  makeURL.findOne({ shortURL })
+    .lean()
+    // 若找不到資料，印出error.hbs頁面
+    .then(data => {
+      if (!data) {
+        const errorMessage = 'Can not found this URL:'
+        const errorURL = req.header.host + '/' + shortURL
+        return res.render('error', { errorMessage, errorURL })
+      }
+    })
 })
 
 
